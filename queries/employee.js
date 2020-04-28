fca5d60b991d046c84e95ad99d8d535a95a7789a
Marc Strong <marcstrong@yahoo.com>
2020-04-28 18:47:50 -0500

    update view all employees to view title of depertment_id

--- queries/employee.js
+++ queries/employee.js
@@ -5,7 +5,7 @@ module.exports = {
     // return all employees ID, first name, last name, title, department ID, salary and manager ID
     getAllEmployees: function(connection){
         // connection.query(`SELECT first_name, CONCAT(first_name,' ' , last_name) AS 'Manager' FROM employee`, function(err, response){
-        return connection.query(`SELECT employee.id, first_name, last_name, title, department_id, salary, manager_id FROM employee INNER JOIN role ON employee.role_id = role.id ORDER BY employee.id;`);
+        return connection.query(`SELECT employee.id, first_name, last_name, title, department.name, salary, manager_id FROM employee INNER JOIN role ON employee.role_id = role.id INNER JOIN department on department.id = role.department_id ORDER BY employee.id;`);
     },
 
     // return all departments
